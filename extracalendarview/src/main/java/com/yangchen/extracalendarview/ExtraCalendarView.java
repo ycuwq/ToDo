57fbc6ab5ede4f3a02a38233e1104d2887c7ea79
yc <ycuwq666@gmail.com>
2017-05-18 22:39:08 +0800

    ###

--- extracalendarview/src/main/java/com/yangchen/extracalendarview/ExtraCalendarView.java
+++ extracalendarview/src/main/java/com/yangchen/extracalendarview/ExtraCalendarView.java
@@ -1,6 +1,7 @@
 package com.yangchen.extracalendarview;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -12,7 +13,27 @@ import android.widget.CalendarView;
  */
 public class ExtraCalendarView extends CalendarView {
 
+	private static final int MAX_ROW = 6;       //最大显示的行数
+	private static final int COLUMN = 7;        //显示的列数
+
+	public ExtraCalendarView(@NonNull Context context) {
+		super(context);
+	}
 	public ExtraCalendarView(@NonNull Context context, @Nullable AttributeSet attrs) {
 		super(context, attrs);
+	}
+
+	public ExtraCalendarView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
 	}
 }
---
+++ extracalendarview/src/main/java/com/yangchen/extracalendarview/data/Date.java
@@ -0,0 +1,80 @@
+package com.yangchen.extracalendarview.data;
+
+/**
+ * Created by 杨晨 on 2017/5/18.
+ */
+public class Date {
+	private int year;
+	private int month;
+	private int day;
+	private int week;
+	private String holiday;         //节假日
+
+	private String lunarMonth;      //农历月
+	private String lunarDay;        //农历日
+	private String lunarHoliday;     //农历节日
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+	public int getDay() {
+		return day;
+	}
+
+	public void setDay(int day) {
+		this.day = day;
+	}
+
+	public int getWeek() {
+		return week;
+	}
+
+	public void setWeek(int week) {
+		this.week = week;
+	}
+
+	public String getHoliday() {
+		return holiday;
+	}
+
+	public void setHoliday(String holiday) {
+		this.holiday = holiday;
+	}
+
+	public String getLunarMonth() {
+		return lunarMonth;
+	}
+
+	public void setLunarMonth(String lunarMonth) {
+		this.lunarMonth = lunarMonth;
+	}
+
+	public String getLunarDay() {
+		return lunarDay;
+	}
+
+	public void setLunarDay(String lunarDay) {
+		this.lunarDay = lunarDay;
+	}
+
+	public String getLunarHoliday() {
+		return lunarHoliday;
+	}
+
+	public void setLunarHoliday(String lunarHoliday) {
+		this.lunarHoliday = lunarHoliday;
+	}
+}
