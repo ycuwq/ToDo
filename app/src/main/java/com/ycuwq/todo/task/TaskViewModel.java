46ae16a48d30d0edc156d485577772473089f265
yc <ycuwq666@gmail.com>
2017-10-18 23:42:56 +0800

    修复日历控件上滑动画失效的问题

--- app/src/main/java/com/ycuwq/todo/task/TaskViewModel.java
+++ app/src/main/java/com/ycuwq/todo/task/TaskViewModel.java
@@ -1,8 +1,8 @@
 package com.ycuwq.todo.task;
 
-import android.databinding.BaseObservable;
 import android.databinding.ObservableField;
 
+import com.ycuwq.todo.app.BaseViewModel;
 import com.ycuwq.todo.data.source.TaskRepository;
 
 import javax.inject.Inject;
@@ -11,7 +11,7 @@ import javax.inject.Inject;
  * Task的ViewModel 利用Dagger2注入TaskRepository
  * Created by 杨晨 on 2017/5/9.
  */
-public class TaskViewModel extends BaseObservable{
+public class TaskViewModel extends BaseViewModel {
 
 	private TaskRepository mTaskRepository;
 	public ObservableField<String> snakeBarText = new ObservableField<>();
